652ebe052cb755d5a2f3a149ffeb914ebb95eb3b
koudai0024 <iwai.0731v@gmail.com>
2021-04-04 16:04:07 +0900

    :art: Add path alias

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require("path")
+
 module.exports = {
   reactStrictMode: true,
   typescript: { ignoreDevErrors: true },
   poweredByHeader: false,
+  webpack(config) {
+    config.resolve.alias["@"] = path.join(__dirname, "src");
+    return config;
+  },
 };
