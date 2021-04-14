c17f8c62269f957befb53a267b3c5d4733a4f4e1
koudai0024 <iwai.0731v@gmail.com>
2021-04-15 01:53:33 +0900

    :art: path aliasの修正

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const path = require("path")
-
 module.exports = {
   reactStrictMode: true,
   typescript: { ignoreDevErrors: true },
   poweredByHeader: false,
-  webpack(config) {
-    config.resolve.alias["@"] = path.join(__dirname, "src");
-    return config;
-  },
 };
